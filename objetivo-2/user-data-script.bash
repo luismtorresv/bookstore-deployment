6b2c8de42f39dc2ed5a4ad1cd424368c45758d1c
Luis Torres <lmtorresv@eafit.edu.co>
2025-11-05 22:52:31 -0500

    Modify user script to use the mount point

--- objetivo-2/user-data-script.bash
+++ objetivo-2/user-data-script.bash
@@ -1,29 +1,29 @@
 #!/bin/bash
-yum install -y amazon-efs-utils nfs-utils git
+yum install -y amazon-efs-utils nfs-utils git python3-pip
 
 FILE_SYSTEM_ID=fs-05144e4f3d335c7a3
 EFS_MOUNT_POINT=/mnt/efs
+APP_DIR=${EFS_MOUNT_POINT}/bookstore
 
+# Prepare and mount EFS
 mkdir -p ${EFS_MOUNT_POINT}
-
-# Mount the EFS
 echo "${FILE_SYSTEM_ID}:/ ${EFS_MOUNT_POINT} efs _netdev,tls 0 0" >> /etc/fstab
 mount -a -t efs defaults
 
-# Clone or copy your Flask app
-cd /home/ec2-user
+# Deploy Flask app to EFS
+cd ${EFS_MOUNT_POINT}
 git clone https://github.com/luismtorresv/bookstore-deployment.git
-cd bookstore-deployment
-cd bookstore
+cd bookstore-deployment/bookstore
+cp -r . ${APP_DIR}
+cd ${APP_DIR}
 
 # Install dependencies
-yum install -y python3-pip
 pip3 install -r requirements.txt
 
 # Environment variables for RDS
 export FLASK_APP=app.py
 export DATABASE_URL="mysql+pymysql://admin:bookstore-deployment@bookstore-deployment-database.c14osnlgmmzl.us-east-1.rds.amazonaws.com:3306/bookstore"
 
-# Start Flask (example via systemd or background process)
+# Run Flask on port 80
 nohup flask run --host=0.0.0.0 --port=80 &
 
